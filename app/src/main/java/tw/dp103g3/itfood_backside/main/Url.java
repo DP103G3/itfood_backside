bcda31f847684e6c2704b5c71c87a95a94a8d6ab
tfpoivic <tfpoivic@gmail.com>
2020-03-12 11:38:35 +0800

    add logo

--- app/src/main/java/tw/dp103g3/itfood_backside/main/Url.java
+++ app/src/main/java/tw/dp103g3/itfood_backside/main/Url.java
@@ -1,5 +1,5 @@
 package tw.dp103g3.itfood_backside.main;
 
 public class Url {
-    public static final String URL = "http://192.168.43.3:8080/Itfood_Web";
+    public static final String URL = "http://172.20.10.2:8080/Itfood_Web";
 }
